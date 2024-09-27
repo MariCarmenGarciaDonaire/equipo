440094b1a2b40bac60ce0d07d6a6302eeb8e7c50
MariCarmenGarciaDonaire <mgardon324@g.educaand.es>
2024-09-27 18:26:41 +0200

    primera modificacion

--- vamos.java
+++ vamos.java
@@ -1,5 +1,6 @@
 public class Vamos {
   public static void main (String[] args) {
     System.out.println("Vamos que nos vamos");
-  }
+	System.out.println("Que nos cierra el supermercado");  
+}
 }
