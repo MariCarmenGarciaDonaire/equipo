7086556a22ee2c005f14a2a2c3a878a758328453
MariCarmenGarciaDonaire <mgardon324@g.educaand.es>
2024-09-27 18:33:06 +0200

    ampliacion

--- vamos.java
+++ vamos.java
@@ -2,5 +2,6 @@ public class Vamos {
   public static void main (String[] args) {
     System.out.println("Vamos que nos vamos");
 	System.out.println("Que nos cierra el supermercado");  
+		System.out.println("Y quiero comprar pan");
 }
 }
